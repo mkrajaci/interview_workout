d369371ea7c665718bf09ed181fa1c61665e9e62
Mario Krajačić <mario@marios-mbp.lan>
2017-06-24 14:05:57 +0200

    My initial set up.

---
+++ Interwiev_projects/Interwiev_projects/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Interwiev_projects
+//
+//  Created by Mario Krajačić on 24/06/2017.
+//  Copyright © 2017 Mario Krajačić. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
