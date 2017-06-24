774c4db745c0935422759f16e95bae4f817eea0d
Mario Krajačić <mario@marios-mbp.lan>
2017-06-24 15:29:27 +0200

    Added new project

---
+++ Interwiev_projects/drugi_project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  drugi_project
+//
+//  Created by Mario Krajačić on 24/06/2017.
+//  Copyright © 2017 Mario Krajačić. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
